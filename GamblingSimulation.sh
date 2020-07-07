8921577b09472574892cc360a822bc5ca4d41099
priyalisakhare31@gmail.com <priyalisakhare31@gmail.com>
2020-07-07 17:42:06 +0530

    [Priyali]Added: Finding lucky and unlucky day of month

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -1,21 +1,20 @@
-#!/bin/bash
+#!/bin/bash 
 echo "gambling simulator"
-
 #constants
 INITIAL=100
 BET=1
-
 #variables
 stakePercentAmount=$(( 50*$INITIAL/100 ))
 maxWin=$(( $stakePercentAmount+$INITIAL ))
 maxLoss=$(( $INITIAL-$stakePercentAmount ))
 numOfDays=30
-loss=0
 win=0
+loss=0
 daysWin=0
 daysLoss=0
 
 declare -A fullDay
+declare -A maxMinDay
 
 function betPlayed()
 {       dayStake=$INITIAL
@@ -32,18 +31,36 @@ function betPlayed()
 }
         for (( day=1; day<=$numOfDays; day++ ))
         do
-        betPlayed
 	fullDay["Day $day"]=$stakePercentAmount
-
+        betPlayed
                 if [ $dayStake -eq $maxLoss ]
                 then
-                        loss=$(( $loss-$stakePercentAmount ))
-			(( daysLoss++ ))
+                        loss=$(($loss - $stakePercentAmount ))
+                        maxMinDay["Day $day"]=$loss
+ 			(( daysLoss++ ))
                 else
-                        win=$(( $win+$stakePercentAmount ))
+                        win=$(( $win + $stakePercentAmount ))
+                        maxMinDay["Day $day"]=$win
 			(( daysWin++ ))
                 fi
         done
 
+echo "Total loss $loss"
+echo "Total win- $win"
+
 echo "Winned days $daysWin by $(($daysWin*$stakePercentAmount))" 
 echo "Lossed days $daysLoss by  $(($daysLoss*$stakePercentAmount))"
+
+luckyDay=$( printf "%s\n" ${maxMinDay[@]} | sort -nr | head -1 )
+unluckyDay=$( printf "%s\n" ${maxMinDay[@]} | sort -nr | tail -1 )
+
+        for data in "${!maxMinDay[@]}"
+        do
+                if [[ ${maxMinDay[$data]} -eq $luckyDay ]]
+                then
+                echo "Lucky Day-" $data
+                elif [[ ${maxMinDay[$data]} -eq $unluckyDay ]]
+                then
+                echo "Unlucky Day-" $data
+                fi
+        done
