1c1ea8e4800b309d18c2be1186997f81c47b694e
priyalisakhare31@gmail.com <priyalisakhare31@gmail.com>
2020-07-07 13:45:37 +0530

    [Priyali]Added:Gambler per day max_win or min_loss

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -1,12 +1,30 @@
-#! /bin/bash/
-
-echo "Welcome To Gambling"
-#Constants
+#!/bin/bash 
+echo "gambling simulator"
+#constants
 INITIAL=100
 BET=1
 
-case $(( $RANDOM%2 )) in
+#variables
+stake=$INITIAL
+stakePercentAmount=$(( 50*$INITIAL/100 ))
+maxWin=$(( $stakePercentAmount+$INITIAL ))
+maxLoss=$(( $INITIAL-$stakePercentAmount ))
 
-	0) echo "player lost the bet" ;;
-	1) echo "player won the bet" ;;
-esac
+function calculate() 
+{
+        while [ $stake -lt $maxWin ] && [ $stake -gt $maxLoss ]
+        do
+        play=$(( RANDOM%2 ))
+
+         if [ $play -eq 1 ]
+         then
+                stake=$(( stake+BET ))
+         else
+                stake=$(( stake-BET ))
+         fi
+        done
+}
+
+calculate
+
+echo "Resign for day"
