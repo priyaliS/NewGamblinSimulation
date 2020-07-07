75d3f7efcf5fe5930c8a100ba6f855aa6ef54085
priyalisakhare31@gmail.com <priyalisakhare31@gmail.com>
2020-07-07 14:14:01 +0530

    [Priyali]Added:Total amount won or loss after 20days

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -3,15 +3,15 @@ echo "gambling simulator"
 #constants
 INITIAL=100
 BET=1
-
-#variables
+#varibles
 stake=$INITIAL
 stakePercentAmount=$(( 50*$INITIAL/100 ))
 maxWin=$(( $stakePercentAmount+$INITIAL ))
 maxLoss=$(( $INITIAL-$stakePercentAmount ))
+numOfDays=20
 
 function calculate() 
-{
+{	stake=$INITIAL
         while [ $stake -lt $maxWin ] && [ $stake -gt $maxLoss ]
         do
         play=$(( RANDOM%2 ))
@@ -25,6 +25,17 @@ function calculate()
         done
 }
 
-calculate
+for (( day=0; day<$numOfDays; day++ ))
+        do
+        calculate
+                if [ $stake -eq $maxLoss ]
+                then
+                loss=$(( loss-50 ))
+                else
+                win=$(( win+50 ))
+                fi 
+        echo "Resign for day"
+        done
 
-echo "Resign for day"
+echo "Total amount Loss"$loss
+echo "Total amount Won" $win
